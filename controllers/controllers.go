b931da049f147a709fa559b20d703349bbf905d1
Diva2504 <64818972+Diva2504@users.noreply.github.com>
2022-04-10 00:18:06 +0800

    create endpoint :  GetAll, GetTodo, GetById, CreateTodo

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,7 +1,9 @@
 package controllers
 
 import (
+	"errors"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/takadev15/todo-apps/models"
@@ -11,21 +13,50 @@ var todos = make([]models.Todos, 0, 10)
 
 // test json
 func TestApi(c *gin.Context) {
-  c.JSON(http.StatusOK, gin.H{
-    "Message": "testing",
-  })
+	c.JSON(http.StatusOK, gin.H{
+		"Message": "testing",
+	})
 }
 
 // swagger
 func GetAll(c *gin.Context) {
+	c.JSON(http.StatusOK, todos)
 }
 
 // swagger
-func GetById(c *gin.Context) {
+func GetTodo(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+	singleTodo, err := GetById(id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"message": "data not found"})
+		return
+	}
+	c.JSON(http.StatusOK, singleTodo)
+}
+func GetById(id int) (*models.Todos, error) {
+
+	for i, t := range todos {
+		if t.Id == id {
+			return &todos[i], nil
+		}
+	}
+	return nil, errors.New("data not found")
 }
 
 // swagger
 func CreateTodo(c *gin.Context) {
+	var reqtodos models.Todos
+
+	err := c.ShouldBindJSON(&reqtodos)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":   true,
+			"message": "invalid request body",
+		})
+		return
+	}
+	todos = append(todos, reqtodos)
+	c.JSON(http.StatusCreated, reqtodos)
 }
 
 // swagger
--- main.go
+++ main.go
@@ -2,21 +2,22 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 	"github.com/takadev15/todo-apps/controllers"
-  docs "github.com/takadev15/todo-apps/docs"
-  swaggerfiles "github.com/swaggo/files"
-  ginSwagger "github.com/swaggo/gin-swagger"
-  )
-
+	docs "github.com/takadev15/todo-apps/docs"
+)
 
 func main() {
-  router := gin.Default()
-  docs.SwaggerInfo.BasePath = "/todos"
-  todoRoutes := router.Group("/todos")
-  {
-    todoRoutes.GET("/test", controllers.TestApi)
-    todoRoutes.GET("/")
-  }
-   router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
-   router.Run(":8080")
+	router := gin.Default()
+	docs.SwaggerInfo.BasePath = "/todos"
+	todoRoutes := router.Group("/todos")
+	{
+		todoRoutes.GET("/test", controllers.TestApi)
+		todoRoutes.GET("/getall", controllers.GetAll)
+		todoRoutes.GET("/getbyid/:id", controllers.GetTodo)
+		todoRoutes.POST("/create", controllers.CreateTodo)
+	}
+	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+	router.Run(":8080")
 }
